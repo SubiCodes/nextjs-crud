6e1bf3e6f84b8bc999ccabe959277c44ebd50662
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-15 20:31:36 +0800

    feat: created the sign in button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
-import { Gpu, Home } from 'lucide-react'
+import { Gpu, Home, LogIn } from 'lucide-react'
 import { ModeToggle } from './ModeToggle'
+import { stackServerApp } from '@/stack'
 
-function Navbar() {
+async function Navbar() {
+
+    const user = await stackServerApp.getUser();
+    const app = stackServerApp.urls;
+
     return (
         <nav className='sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50'>
             <div className='max-w-7xl mx-auto px-4'>
@@ -35,6 +40,14 @@ function Navbar() {
                         </Button>
 
                         <ModeToggle/>
+
+                        {/* Sign In Button from StackAuth */}
+                        <Button variant={"ghost"} className='flex items-center gap-2' asChild>
+                            <Link href={app.signIn}>
+                                <LogIn className='w-4 h-4' />
+                                <span className='hidden lg:inline'>{`Sign In`}</span>
+                            </Link>
+                        </Button>
 
                     </div>
 
