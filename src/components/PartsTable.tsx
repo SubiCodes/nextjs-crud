23502653fb707bbd716fdbb82c38d4f78ed0d8a2
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-16 16:35:45 +0800

    feat: called the diaog in the table file

--- src/components/PartsTable.tsx
+++ src/components/PartsTable.tsx
@@ -21,6 +21,7 @@ import { useRouter } from "next/navigation";
 import { getCurrentUserId } from "@/actions/user.actions";
 import { SignIn } from "@stackframe/stack";
 import { Skeleton } from "./ui/skeleton";
+import AddDialog from "./AddDialog";
 
 type GetPartsResult = Awaited<ReturnType<typeof getParts>>;
 
@@ -59,7 +60,7 @@ export default function TableDemo({ parts, user }: PartsTableProps) {
         <Input placeholder={"Filter PC Parts..."} className="pl-10" onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} />
         <Search className="absolute h-4 w-4 left-3 top-1/2 transform -translate-y-1/2" />
         <Combobox value={category} onChange={(val) => setCategory(val)} />
-        <Button variant={'default'}>Add Part</Button>
+        <AddDialog/>
       </div>
       <Table>
         <TableCaption className="text-sm text-muted-foreground">
