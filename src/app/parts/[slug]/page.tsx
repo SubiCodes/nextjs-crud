e89ceab280660a035ebd1252e97418b95f8dbdec
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-16 16:02:09 +0800

    feat: added a meta data on the view part page

--- src/app/parts/[slug]/page.tsx
+++ src/app/parts/[slug]/page.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 import PartCard from './PartCard'
 import { getPart } from '@/actions/part.action'
 
+export async function generateMetadata({
+    params,
+  }: {
+    params: { slug: string };
+  }) {
+   
+    // Extract the id from the slug by splitting on the delimiter
+    const [id] = params.slug.split("--");
+    const part = await getPart(id);
+    return {
+      title: part ? part.userPart?.name : "Part Details",
+      description: part ? part.userPart?.description : "Part details page",
+    };
+}
+
 async function page({params}: {params: {slug: string}}) {
 
     const [id] = params.slug.split("--");
