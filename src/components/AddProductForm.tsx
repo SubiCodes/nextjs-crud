b402f75cf3ac59122a6bfa9a7345a36dd3f070f6
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-18 21:37:42 +0800

    fix: the select in the form

--- src/components/AddProductForm.tsx
+++ src/components/AddProductForm.tsx
@@ -1,193 +1,198 @@
 "use client"
 
 import {
-  useState
+    useState
 } from "react"
 import {
-  toast
+    toast
 } from "sonner"
 import {
-  useForm
+    useForm
 } from "react-hook-form"
 import {
-  zodResolver
+    zodResolver
 } from "@hookform/resolvers/zod"
 import {
-  z
+    z
 } from "zod"
 import {
-  cn
+    cn
 } from "@/lib/utils"
 import {
-  Button
+    Button
 } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form"
 import {
-  Input
+    Input
 } from "@/components/ui/input"
 import {
-  Textarea
+    Textarea
 } from "@/components/ui/textarea"
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
 } from "@/components/ui/select"
 
 const formSchema = z.object({
-  name: z.string().min(1),
-  description: z.string(),
-  type: z.string(),
-  brand: z.string().min(1),
-  imageUrl: z.string().min(1)
+    name: z.string().min(1),
+    description: z.string(),
+    type: z.string(),
+    brand: z.string().min(1),
+    imageUrl: z.string().min(1)
 });
 
 export default function AddProductForm() {
 
-  const form = useForm < z.infer < typeof formSchema >> ({
-    resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
 
-  })
+    })
 
-  function onSubmit(values: z.infer < typeof formSchema > ) {
-    try {
-      console.log(values);
-      toast(
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-        </pre>
-      );
-    } catch (error) {
-      console.error("Form submission error", error);
-      toast.error("Failed to submit the form. Please try again.");
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            console.log(values);
+            toast(
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+                </pre>
+            );
+        } catch (error) {
+            console.error("Form submission error", error);
+            toast.error("Failed to submit the form. Please try again.");
+        }
     }
-  }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
-        
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Product Name</FormLabel>
-              <FormControl>
-                <Input 
-                placeholder=""
-                
-                type=""
-                {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder=""
-                  className="resize-none"
-                  {...field}
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Product Name</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder=""
+
+                                    type=""
+                                    {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        
-        <div className="grid grid-cols-12 gap-4">
-          
-          <div className="col-span-6">
-            
-        <FormField
-          control={form.control}
-          name="type"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Type</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="m@example.com">m@example.com</SelectItem>
-                  <SelectItem value="m@google.com">m@google.com</SelectItem>
-                  <SelectItem value="m@support.com">m@support.com</SelectItem>
-                </SelectContent>
-              </Select>
-                
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-          </div>
-          
-          <div className="col-span-6">
-            
-        <FormField
-          control={form.control}
-          name="brand"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Brand</FormLabel>
-              <FormControl>
-                <Input 
-                placeholder=""
-                
-                type="text"
-                {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-          </div>
-          
-        </div>
-        
-        <FormField
-          control={form.control}
-          name="imageUrl"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Image URL</FormLabel>
-              <FormControl>
-                <Input 
-                placeholder=""
-                
-                type="text"
-                {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="w-full">Submit</Button>
-      </form>
-    </Form>
-  )
+
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    placeholder=""
+                                    className="resize-none"
+                                    {...field}
+                                />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <div className="grid grid-cols-12 gap-4">
+
+                    <div className="col-span-6">
+
+                        <FormField
+                            control={form.control}
+                            name="type"
+                            render={({ field }) => (
+                                <FormItem className="w-full">
+                                    <FormLabel>Type</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <FormControl>
+                                            <SelectTrigger className="w-full">
+                                                <SelectValue placeholder="" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value="cpu">CPU</SelectItem>
+                                            <SelectItem value="cpu cooler">CPU Cooler</SelectItem>
+                                            <SelectItem value="motherboard">Motherboard</SelectItem>
+                                            <SelectItem value="memory">Memory</SelectItem>
+                                            <SelectItem value="storage">Storage</SelectItem>
+                                            <SelectItem value="gpu">GPU</SelectItem>
+                                            <SelectItem value="power supply">Power Supply</SelectItem>
+                                            <SelectItem value="cases">Cases</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className="col-span-6">
+
+                        <FormField
+                            control={form.control}
+                            name="brand"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Brand</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder=""
+
+                                            type="text"
+                                            {...field} />
+                                    </FormControl>
+
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                </div>
+
+                <FormField
+                    control={form.control}
+                    name="imageUrl"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Image URL</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder=""
+
+                                    type="text"
+                                    {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="w-full">Submit</Button>
+            </form>
+        </Form>
+    )
 }
